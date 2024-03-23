0f8d607e0cece863a55ea837b1c9cf2b93065531
Kaio Sérgio Sales Nunes <137112682+Kaiosergio21@users.noreply.github.com>
2024-03-23 12:14:35 -0300

    Add files via upload

---
+++ relogio/JAVASCRIPT/script.js
@@ -0,0 +1,68 @@
+const horas = document.querySelector("#horas");
+const minutos = document.querySelector("#minutos");
+const segundos = document.querySelector("#segundos");
+const dia = document.querySelector("#dia");
+const mes = document.querySelector("#mes");
+const ano = document.querySelector("#ano");
+const boa = document.querySelector("#boa"); // Assuming boa is the element where you want to display the greeting
+const back = document.querySelector("#back"); // Assuming container is the element where you want to change the background
+
+//event
+setInterval(relogio, 1000);
+
+//função
+function relogio() {
+    let agora = new Date();
+    let h = agora.getHours();
+    let m = agora.getMinutes();
+    let s = agora.getSeconds();
+    let dd = agora.getDate();
+    let mm = agora.getMonth() + 1;
+    let yy = agora.getFullYear();
+
+    horas.textContent = h;
+    minutos.textContent = m;
+    segundos.textContent = s;
+
+    if (h < 10) {
+        horas.textContent = "0" + h;
+    }
+    if (m < 10) {
+        minutos.textContent = "0" + m;
+    }
+    if (s < 10) {
+        segundos.textContent = "0" + s;
+    }
+
+    dia.textContent = dd;
+    mes.textContent = mm;
+    ano.textContent = yy;
+
+    if (mm <= 10) {
+        mes.textContent = "0" + mm;
+    }
+
+    if (dd < 10) {
+        dd = "0" + dd;
+    }
+    if (mm < 10) {
+        mm = "00" + mm;
+    }
+
+    let bom;
+    let imageurl;
+
+    if (h >= 5 && h < 12) {
+        bom = "bom dia";
+        imageurl ="../images/sky.jpg";
+    } else if (h >= 12 && h < 18) {
+        bom = "boa tarde";
+        imageurl = "../images/por_do_sol.jpeg";
+    } else {
+        bom = "boa noite";
+        imageurl = "../images/lua_cheia.jpg";
+    }
+
+    boa.textContent = bom;
+    back.style.backgroundImage = "url('" + imageurl + "')";
+}
